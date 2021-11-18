d98d669cf8aafd339a83ad13576989176afd0a84
Pedro Henrique Vasconcelos <pedroh.cpv@hotmail.com>
2021-11-18 17:14:04 -0300

    Continuando a limpeza e instalando a biblioteca styled components e criando estilo global da aplicacao

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
+import Home from './pages/Home';
+
+const App: React.FC = () => {
+    return (
+        <>
+            <GlobalStyles />
+            <Home />
+        </>
+    );
+}
+
+export default App;
